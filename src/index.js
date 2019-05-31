32bb22e464ec4134eb73d8461d4b0ce531234086
David Furnes <david@dfurnes.com>
2019-05-31 13:03:39 -0400

    MVP component with simple preview.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import { TextInput } from '@contentful/forma-36-react-components';
+import { format, parseISO } from 'date-fns';
 import { init } from 'contentful-ui-extensions-sdk';
+import { ApolloProvider, Query } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+import { gql } from 'apollo-boost';
+import {
+  FieldGroup,
+  TextInput,
+  TextLink,
+  EntryCard,
+  Note,
+} from '@contentful/forma-36-react-components';
+
 import '@contentful/forma-36-react-components/dist/styles.css';
 import './index.css';
+
+const graphql = new ApolloClient({
+  uri: 'https://graphql.dosomething.org',
+});
+
+const CAMPAIGN_QUERY = gql`
+  query ContentfulCampaignExtension($id: Int!) {
+    campaign(id: $id) {
+      internalTitle
+      startDate
+      endDate
+    }
+  }
+`;
 
 class App extends React.Component {
   static propTypes = {
@@ -25,7 +50,7 @@ class App extends React.Component {
 
     // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
     this.detachExternalChangeHandler = this.props.sdk.field.onValueChanged(
-      this.onExternalChange
+      this.onExternalChange,
     );
   }
 
@@ -50,14 +75,57 @@ class App extends React.Component {
   };
 
   render() {
+    const id = Number(this.state.value);
+
     return (
-      <TextInput
-        width="large"
-        type="text"
-        id="my-field"
-        value={this.state.value}
-        onChange={this.onChange}
-      />
+      <ApolloProvider client={graphql}>
+        <FieldGroup row={true} style={{ alignItems: 'center' }}>
+          <TextInput
+            type="text"
+            width="large"
+            id="campaign-id"
+            placeholder="Paste a campaign ID here (e.g. 9001)..."
+            value={this.state.value}
+            onChange={this.onChange}
+          />
+          <TextLink
+            icon="ChevronRight"
+            href="https://activity.dosomething.org/campaign-ids"
+          >
+            Find a campaign ID...
+          </TextLink>
+        </FieldGroup>
+        <Query query={CAMPAIGN_QUERY} variables={{ id }}>
+          {({ loading, error, data }) => {
+            const campaign = data.campaign;
+
+            if (loading) {
+              return <EntryCard isLoading={loading} />;
+            }
+
+            if (!campaign || error) {
+              return (
+                <Note noteType="warning">This isn't a valid campaign ID.</Note>
+              );
+            }
+
+            const startDate = format(parseISO(campaign.startDate), 'PP');
+            const endDate = campaign.endDate
+              ? format(parseISO(campaign.endDate), 'PP')
+              : 'forever';
+
+            return (
+              <EntryCard
+                title={campaign.internalTitle}
+                contentType={`${startDate} – ${endDate}`}
+                href={`https://activity.dosomething.org/campaign-ids/${id}`}
+                onClick={() => alert('EntityListItem onClick')}
+                size="small"
+              />
+            );
+          }}
+        </Query>
+      </ApolloProvider>
     );
   }
 }
@@ -65,11 +133,3 @@ class App extends React.Component {
 init(sdk => {
   ReactDOM.render(<App sdk={sdk} />, document.getElementById('root'));
 });
-
-/**
- * By default, iframe of the extension is fully reloaded on every save of a source file.
- * If you want to use HMR (hot module reload) instead of full reload, uncomment the following lines
- */
-// if (module.hot) {
-//   module.hot.accept();
-// }
